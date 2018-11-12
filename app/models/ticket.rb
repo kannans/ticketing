bde5f425cc6c8cf22c04a00313da0310289c2972
kannans <kannanhudson@gmail.com>
2018-11-12 09:27:02 +0530

    Plot polygon data on google MAP

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,11 @@ class Ticket < ApplicationRecord
   serialize :service_area, JSON
   has_one :excavator
   accepts_nested_attributes_for :excavator, reject_if: ->(attributes){ attributes['company_name'].blank? }, allow_destroy: true
+
+
+  def map_coordinates
+    co_text = digsite_info && digsite_info.tr('POLYGON((', '').tr(")", '') || []
+    co = co_text.split(",")
+    co.map { |x| x.split(" ") }.map { |x| { lat: x.first, lng: x.last } }
+  end
 end
